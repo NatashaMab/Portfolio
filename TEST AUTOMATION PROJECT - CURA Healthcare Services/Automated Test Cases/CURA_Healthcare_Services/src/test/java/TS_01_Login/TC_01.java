9da26e5fe1fa799c9fef8578b404a3f901874647
NatashaMab <mabairenatasha@gmail.com>
2025-07-12 17:04:27 +0200

    trying to get the CURA project working

--- TEST AUTOMATION PROJECT - CURA Healthcare Services/Automated Test Cases/CURA_Healthcare_Services/src/test/java/TS_01_Login/TC_01.java
+++ TEST AUTOMATION PROJECT - CURA Healthcare Services/Automated Test Cases/CURA_Healthcare_Services/src/test/java/TS_01_Login/TC_01.java
@@ -1,7 +1,9 @@
 package TS_01_Login;
 
 public class TC_01 {
- public static void main(String[] args) {
+	
+  public static void main(String[] args) {
 	 System.out.println("why is there an question mark");
+	 
  }
 }
